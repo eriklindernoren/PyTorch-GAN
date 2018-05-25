b8ea7d671e5840d226b01081aec1aba9a08dd8cf
eriklindernoren <eriklindernoren@gmail.com>
2018-05-25 22:41:54 +0200

    MUNIT: Multimodal Unsupervised Image-to-Image Translation

--- README.md
+++ README.md
@@ -26,6 +26,7 @@ Collection of PyTorch implementations of Generative Adversarial Network varietie
     + [GAN](#gan)
     + [InfoGAN](#infogan)
     + [Least Squares GAN](#least-squares-gan)
+    + [MUNIT](#munit)
     + [Pix2Pix](#pix2pix)
     + [PixelDA](#pixelda)
     + [Semi-Supervised GAN](#semi-supervised-gan)
@@ -424,6 +425,26 @@ Unsupervised learning with generative adversarial networks (GANs) has proven hug
 ```
 $ cd implementations/lsgan/
 $ python3 lsgan.py
+```
+
+
+### MUNIT
+_Multimodal Unsupervised Image-to-Image Translation_
+
+#### Authors
+Xun Huang, Ming-Yu Liu, Serge Belongie, Jan Kautz
+
+#### Abstract
+Unsupervised image-to-image translation is an important and challenging problem in computer vision. Given an image in the source domain, the goal is to learn the conditional distribution of corresponding images in the target domain, without seeing any pairs of corresponding images. While this conditional distribution is inherently multimodal, existing approaches make an overly simplified assumption, modeling it as a deterministic one-to-one mapping. As a result, they fail to generate diverse outputs from a given source domain image. To address this limitation, we propose a Multimodal Unsupervised Image-to-image Translation (MUNIT) framework. We assume that the image representation can be decomposed into a content code that is domain-invariant, and a style code that captures domain-specific properties. To translate an image to another domain, we recombine its content code with a random style code sampled from the style space of the target domain. We analyze the proposed framework and establish several theoretical results. Extensive experiments with comparisons to the state-of-the-art approaches further demonstrates the advantage of the proposed framework. Moreover, our framework allows users to control the style of translation outputs by providing an example style image. Code and pretrained models are available at [this https URL](https://github.com/nvlabs/MUNIT)
+
+[[Paper]](https://arxiv.org/abs/1804.04732) [[Code]](implementations/munit/munit.py)
+
+#### Run Example
+```
+$ cd data/
+$ bash download_pix2pix_dataset.sh edges2shoes
+$ cd ../implementations/munit/
+$ python3 munit.py --dataset_name edges2shoes
 ```
 
 ### Pix2Pix
---
+++ implementations/munit/models.py
@@ -0,0 +1,290 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+from torch.autograd import Variable
+import numpy as np
+
+def weights_init_normal(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm2d') != -1:
+        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0.0)
+
+class LambdaLR():
+    def __init__(self, n_epochs, offset, decay_start_epoch):
+        assert ((n_epochs - decay_start_epoch) > 0), "Decay must start before the training session ends!"
+        self.n_epochs = n_epochs
+        self.offset = offset
+        self.decay_start_epoch = decay_start_epoch
+
+    def step(self, epoch):
+        return 1.0 - max(0, epoch + self.offset - self.decay_start_epoch)/(self.n_epochs - self.decay_start_epoch)
+
+
+#################################
+#           Encoder
+#################################
+
+class Encoder(nn.Module):
+    def __init__(self, in_channels=3, dim=64, n_residual=3, n_downsample=2, style_dim=8):
+        super(Encoder, self).__init__()
+        self.content_encoder = ContentEncoder(in_channels, dim, n_residual, n_downsample)
+        self.style_encoder = StyleEncoder(in_channels, dim, n_downsample, style_dim)
+
+    def forward(self, x):
+        content_code = self.content_encoder(x)
+        style_code = self.style_encoder(x)
+        return content_code, style_code
+
+#################################
+#            Decoder
+#################################
+
+class Decoder(nn.Module):
+    def __init__(self, out_channels=3, dim=64, n_residual=3, n_upsample=2, style_dim=8):
+        super(Decoder, self).__init__()
+
+        layers = []
+        dim = dim * 2**n_upsample
+        # Residual blocks
+        for _ in range(n_residual):
+            layers += [ResidualBlock(dim, norm='adain')]
+
+        # Upsampling
+        for _ in range(n_upsample):
+            layers += [ nn.Upsample(scale_factor=2),
+                        nn.Conv2d(dim, dim // 2, 5, stride=1, padding=2),
+                        LayerNorm(dim // 2),
+                        nn.LeakyReLU(0.2, inplace=True) ]
+            dim = dim // 2
+
+        # Output layer
+        layers += [ nn.ReflectionPad2d(3),
+                    nn.Conv2d(dim, out_channels, 7),
+                    nn.Tanh() ]
+
+        self.model = nn.Sequential(*layers)
+
+        # Initiate mlp (predicts AdaIN parameters)
+        num_adain_params = self.get_num_adain_params()
+        self.mlp = MLP(style_dim, num_adain_params)
+
+    def get_num_adain_params(self):
+        """Return the number of AdaIN parameters needed by the model"""
+        num_adain_params = 0
+        for m in self.modules():
+            if m.__class__.__name__ == "AdaptiveInstanceNorm2d":
+                num_adain_params += 2 * m.num_features
+        return num_adain_params
+
+    def assign_adain_params(self, adain_params):
+        """Assign the adain_params to the AdaIN layers in model"""
+        for m in self.modules():
+            if m.__class__.__name__ == "AdaptiveInstanceNorm2d":
+                # Extract mean and std predictions
+                mean = adain_params[:, :m.num_features]
+                std = adain_params[:, m.num_features:2*m.num_features]
+                # Update bias and weight
+                m.bias = mean.contiguous().view(-1)
+                m.weight = std.contiguous().view(-1)
+                # Move pointer
+                if adain_params.size(1) > 2*m.num_features:
+                    adain_params = adain_params[:, 2*m.num_features:]
+
+    def forward(self, content_code, style_code):
+        # Update AdaIN parameters by MLP prediction based off style code
+        self.assign_adain_params(self.mlp(style_code))
+        img = self.model(content_code)
+        return img
+
+
+#################################
+#        Content Encoder
+#################################
+
+class ContentEncoder(nn.Module):
+    def __init__(self, in_channels=3, dim=64, n_residual=3, n_downsample=2):
+        super(ContentEncoder, self).__init__()
+
+        # Initial convolution block
+        layers = [  nn.ReflectionPad2d(3),
+                    nn.Conv2d(in_channels, dim, 7),
+                    nn.InstanceNorm2d(64),
+                    nn.LeakyReLU(0.2, inplace=True) ]
+
+        # Downsampling
+        for _ in range(n_downsample):
+            layers += [ nn.Conv2d(dim, dim * 2, 4, stride=2, padding=1),
+                        nn.InstanceNorm2d(dim * 2),
+                        nn.ReLU(inplace=True) ]
+            dim *= 2
+
+        # Residual blocks
+        for _ in range(n_residual):
+            layers += [ResidualBlock(dim, norm='in')]
+
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
+
+#################################
+#        Style Encoder
+#################################
+
+class StyleEncoder(nn.Module):
+    def __init__(self, in_channels=3, dim=64, n_downsample=2, style_dim=8):
+        super(StyleEncoder, self).__init__()
+
+        # Initial conv block
+        layers = [  nn.ReflectionPad2d(3),
+                    nn.Conv2d(in_channels, dim, 7),
+                    nn.LeakyReLU(0.2, inplace=True) ]
+
+        # Downsampling
+        for _ in range(2):
+            layers += [nn.Conv2d(dim, dim * 2, 4, stride=2, padding=1), nn.ReLU(inplace=True)]
+            dim *= 2
+
+        # Downsampling with constant depth
+        for _ in range(n_downsample - 2):
+            layers += [nn.Conv2d(dim, dim, 4, stride=2, padding=1), nn.ReLU(inplace=True)]
+
+        # Average pool and output layer
+        layers += [ nn.AdaptiveAvgPool2d(1),
+                    nn.Conv2d(dim, style_dim, 1, 1, 0)]
+
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+######################################
+#   MLP (predicts AdaIn parameters)
+######################################
+
+class MLP(nn.Module):
+    def __init__(self, input_dim, output_dim, dim=256, n_blk=3, activ='relu'):
+        super(MLP, self).__init__()
+        layers = [nn.Linear(input_dim, dim), nn.ReLU(inplace=True)]
+        for _ in range(n_blk - 2):
+            layers += [nn.Linear(dim, dim), nn.ReLU(inplace=True)]
+        layers += [nn.Linear(dim, output_dim)]
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x.view(x.size(0), -1))
+
+
+##############################
+#        Discriminator
+##############################
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels=3):
+        super(Discriminator, self).__init__()
+
+        def discriminator_block(in_filters, out_filters, normalize=True):
+            """Returns downsampling layers of each discriminator block"""
+            layers = [nn.Conv2d(in_filters, out_filters, 4, stride=2, padding=1)]
+            if normalize:
+                layers.append(nn.InstanceNorm2d(out_filters))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.model = nn.Sequential(
+            *discriminator_block(in_channels, 64, normalize=False),
+            *discriminator_block(64, 128),
+            *discriminator_block(128, 256),
+            *discriminator_block(256, 512),
+            nn.Conv2d(512, 1, 3, padding=1)
+        )
+
+    def forward(self, img):
+        return self.model(img)
+
+
+##############################
+#       Custom Blocks
+##############################
+
+class ResidualBlock(nn.Module):
+    def __init__(self, features, norm='in'):
+        super(ResidualBlock, self).__init__()
+
+        norm_layer = AdaptiveInstanceNorm2d if norm == 'adain' else nn.InstanceNorm2d
+
+        conv_block = [  nn.ReflectionPad2d(1),
+                        nn.Conv2d(features, features, 3),
+                        norm_layer(features),
+                        nn.ReLU(inplace=True),
+                        nn.ReflectionPad2d(1),
+                        nn.Conv2d(features, features, 3),
+                        norm_layer(features)  ]
+
+        self.conv_block = nn.Sequential(*conv_block)
+
+    def forward(self, x):
+        return x + self.conv_block(x)
+
+
+##############################
+#        Custom Layers
+##############################
+
+class AdaptiveInstanceNorm2d(nn.Module):
+    """Reference: https://github.com/NVlabs/MUNIT/blob/master/networks.py"""
+    def __init__(self, num_features, eps=1e-5, momentum=0.1):
+        super(AdaptiveInstanceNorm2d, self).__init__()
+        self.num_features = num_features
+        self.eps = eps
+        self.momentum = momentum
+        # weight and bias are dynamically assigned
+        self.weight = None
+        self.bias = None
+        # just dummy buffers, not used
+        self.register_buffer('running_mean', torch.zeros(num_features))
+        self.register_buffer('running_var', torch.ones(num_features))
+
+    def forward(self, x):
+        assert self.weight is not None and self.bias is not None, "Please assign weight and bias before calling AdaIN!"
+        b, c, h, w = x.size()
+        running_mean = self.running_mean.repeat(b)
+        running_var = self.running_var.repeat(b)
+
+        # Apply instance norm
+        x_reshaped = x.contiguous().view(1, b * c, h, w)
+
+        out = F.batch_norm(
+            x_reshaped, running_mean, running_var, self.weight, self.bias,
+            True, self.momentum, self.eps)
+
+        return out.view(b, c, h, w)
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(' + str(self.num_features) + ')'
+
+class LayerNorm(nn.Module):
+    def __init__(self, num_features, eps=1e-5, affine=True):
+        super(LayerNorm, self).__init__()
+        self.num_features = num_features
+        self.affine = affine
+        self.eps = eps
+
+        if self.affine:
+            self.gamma = nn.Parameter(torch.Tensor(num_features).uniform_())
+            self.beta = nn.Parameter(torch.zeros(num_features))
+
+    def forward(self, x):
+        shape = [-1] + [1] * (x.dim() - 1)
+        mean = x.view(x.size(0), -1).mean(1).view(*shape)
+        std = x.view(x.size(0), -1).std(1).view(*shape)
+        x = (x - mean) / (std + self.eps)
+
+        if self.affine:
+            shape = [1, -1] + [1] * (x.dim() - 2)
+            x = x * self.gamma.view(*shape) + self.beta.view(*shape)
+        return x
