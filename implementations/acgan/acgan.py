c80d8bc1951cd8c6fb1856314f38d143a271f1f4
Yunzhong Hou <hou_yz@outlook.com>
2020-03-24 22:38:06 +1100

    ACGAN: remove softmax for aux_layer
    torch.nn.CrossEntropyLoss() does not need softmax activation.

--- implementations/acgan/acgan.py
+++ implementations/acgan/acgan.py
@@ -97,7 +97,7 @@ class Discriminator(nn.Module):
 
         # Output layers
         self.adv_layer = nn.Sequential(nn.Linear(128 * ds_size ** 2, 1), nn.Sigmoid())
-        self.aux_layer = nn.Sequential(nn.Linear(128 * ds_size ** 2, opt.n_classes), nn.Softmax())
+        self.aux_layer = nn.Linear(128 * ds_size ** 2, opt.n_classes)
 
     def forward(self, img):
         out = self.conv_blocks(img)
