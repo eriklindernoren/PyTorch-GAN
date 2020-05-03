ab2fa4cca9dc79e7a21999253a15943795fe1aee
Pierre Bernard <pierre.bernard@insa-lyon.fr>
2020-05-03 12:49:22 +0200

    Gan: Allow gpu selection with a new command line argument named "gpu"

--- implementations/gan/gan.py
+++ implementations/gan/gan.py
@@ -27,6 +27,7 @@ parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality 
 parser.add_argument("--img_size", type=int, default=28, help="size of each image dimension")
 parser.add_argument("--channels", type=int, default=1, help="number of image channels")
 parser.add_argument("--sample_interval", type=int, default=400, help="interval betwen image samples")
+parser.add_argument("--gpu", type=int, default=0, help="interval betwen image samples")
 opt = parser.parse_args()
 print(opt)
 
@@ -89,9 +90,13 @@ generator = Generator()
 discriminator = Discriminator()
 
 if cuda:
+    torch.cuda.set_device(opt.gpu)
+    print("using GPU " + str(opt.gpu) + ": " + torch.cuda.get_device_name(opt.gpu))
     generator.cuda()
     discriminator.cuda()
     adversarial_loss.cuda()
+else:
+    print("using CPU")
 
 # Configure data loader
 os.makedirs("../../data/mnist", exist_ok=True)
