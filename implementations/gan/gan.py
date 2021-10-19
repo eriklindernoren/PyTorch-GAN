3136c738f46a6ba7aacb8d65e1ccd2da5018e569
Daisy-bit <2605801303@qq.com>
2021-10-19 21:48:07 +0800

    GAN

--- implementations/gan/gan.py
+++ implementations/gan/gan.py
@@ -23,9 +23,11 @@ parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rat
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+# 噪声z所在的空间，隐空间
 parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
 parser.add_argument("--img_size", type=int, default=28, help="size of each image dimension")
 parser.add_argument("--channels", type=int, default=1, help="number of image channels")
+# 生成器生成图片的周期
 parser.add_argument("--sample_interval", type=int, default=400, help="interval betwen image samples")
 opt = parser.parse_args()
 print(opt)
@@ -51,6 +53,7 @@ class Generator(nn.Module):
             *block(128, 256),
             *block(256, 512),
             *block(512, 1024),
+            # np.prod 用来计算所有元素的乘积
             nn.Linear(1024, int(np.prod(img_shape))),
             nn.Tanh()
         )
@@ -81,7 +84,7 @@ class Discriminator(nn.Module):
         return validity
 
 
-# Loss function
+# Loss function  https://blog.csdn.net/qq_22210253/article/details/85222093
 adversarial_loss = torch.nn.BCELoss()
 
 # Initialize generator and discriminator
@@ -119,13 +122,16 @@ Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 # ----------
 
 for epoch in range(opt.n_epochs):
+    # imgs对应的是图片，_对应的是标签，GAN做的不是分类，我们不会用到分类标签，只需要知道他们的真伪即可
     for i, (imgs, _) in enumerate(dataloader):
 
         # Adversarial ground truths
+        # 64行1列的向量, 这里给的就是真实性评价标签
         valid = Variable(Tensor(imgs.size(0), 1).fill_(1.0), requires_grad=False)
         fake = Variable(Tensor(imgs.size(0), 1).fill_(0.0), requires_grad=False)
 
         # Configure input
+        # 将真实的图片转化为神经网络可以处理的变量
         real_imgs = Variable(imgs.type(Tensor))
 
         # -----------------
@@ -137,10 +143,16 @@ for epoch in range(opt.n_epochs):
         # Sample noise as generator input
         z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
 
-        # Generate a batch of images
+        # Generate a batch of fake images
         gen_imgs = generator(z)
 
         # Loss measures generator's ability to fool the discriminator
+        # 这里可以有两种解释：
+        # 1. G和D本身具有博弈对抗，D想要最大可能地识别出真假数据，
+        # 而G想要尽可能地产生使D混淆无法分辨的图片，故这里也应该与真实标签做对比
+        # 2. 从数学角度来看，D要使价值函数最大得到v(D,G^*)，而G要使v(D,G^*)最小化，
+        # 但是v(D,G^*)前半段不涉及G，故只需要优化后半段，而后半段想要得到最小值需要
+        # 使D(G(z^i))最大，想要使D(G(z^i))最大则要和真实标签做对比，故这里是valid
         g_loss = adversarial_loss(discriminator(gen_imgs), valid)
 
         g_loss.backward()
@@ -151,11 +163,13 @@ for epoch in range(opt.n_epochs):
         # ---------------------
 
         optimizer_D.zero_grad()
-
+        
         # Measure discriminator's ability to classify real from generated samples
         real_loss = adversarial_loss(discriminator(real_imgs), valid)
+        # detach_()是对本身的更改，detach()则是生成了一个新的variable，detach是令tensor的requires_grad为False
         fake_loss = adversarial_loss(discriminator(gen_imgs.detach()), fake)
         d_loss = (real_loss + fake_loss) / 2
+
 
         d_loss.backward()
         optimizer_D.step()
